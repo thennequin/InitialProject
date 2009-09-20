c890dbe6a4fe8e68698b1d6594661245472792a0
cameleonth <cameleonth@9ba7cb8f-89f9-4171-b643-8b30d4d3aa91>
2009-09-20 15:14:10 +0000

    -RessourceDialog return the relative path of file from current working directory. -Set current directory of file dialog to the current working directory in Phazer.

--- trunk/src/Tools/Frames/RessourceDialog.cpp
+++ trunk/src/Tools/Frames/RessourceDialog.cpp
@@ -5,6 +5,20 @@ BEGIN_EVENT_TABLE(RessourceDialog, wxDialog)
 	EVT_COMMAND(wxID_ANY, SelectedRessourceEvent, RessourceDialog::OnSelected)
 END_EVENT_TABLE()
 
+wxString RelativeToPath(wxString filepath, wxString file)
+{
+	int pos = filepath.Last('/');
+	int pos2 = filepath.Last('\\');
+	if (pos2>pos)
+		pos=pos2;
+
+	wxString path = filepath.Left(pos2);
+	if (file.Find(path)!=-1)
+		file = file.Right(file.Length()-path.Length()-1);
+
+	return file;
+}
+
 RessourceDialog::RessourceDialog(wxWindow *parent)
 : wxDialog(parent,wxID_ANY,"Ressource Explorer",wxDefaultPosition,wxDefaultSize,wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER | wxMAXIMIZE_BOX)
 {
@@ -24,11 +38,17 @@ void RessourceDialog::OnSelected(wxCommandEvent& event)
 	//m_aSelected
 	m_aSelected.Clear();
 	wxRessourceItemArray& list = m_pPanel->GetSelection();
+	
+	char buffer[2048];
+	GetCurrentDirectory(2048,buffer);
+
 	for (int i=0;i<list.Count();i++)
 	{
 		//printf("%s\n",list[i]->GetPath().c_str());
 		//printf("%s\n",list[i]->GetFilename().c_str());
-		m_aSelected.Add(wxString(list[i]->GetPath()+"\\"+list[i]->GetFilename()));
+		wxString relativePath = RelativeToPath(buffer,list[i]->GetPath()+"\\"+list[i]->GetFilename());
+		//m_aSelected.Add(wxString(list[i]->GetPath()+"\\"+list[i]->GetFilename()));
+		m_aSelected.Add(wxString(relativePath.c_str()));
 	}
 	EndModal(1);
 }
